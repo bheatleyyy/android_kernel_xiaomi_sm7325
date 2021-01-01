0351a9a04c637ef5e24ef253d943773b9f82519d
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-31 20:44:08 -0800

    kernel: Boost DDR bus for a short period when zygote forks
    There is a gap from when a new app is forked to the time that the
    devfreq governor managing the DDR bus can pick up the increased load and
    ramp up appropriately. Since the polling interval for this governor is
    typically 50 ms, that means it may take up to 50 ms before the governor
    ramps up, which hurts cold launch app performance. Boosting the DDR bus
    for 50 ms upon a fork from zygote covers that gap, speeding up cold app
    launches.
    
    [ghostrider-reborn]
    - add input timeout of 1.5s since we don't want to boost DDR for
    zygote forks not initiated by user input, such as services
    spawning in background
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -85,6 +85,15 @@ static struct task_struct *fp_daemon;
 #define MI_FP_DAEMON_PREFIX "/vendor/bin/hw/mfp-daemon"
 #define FP_DAEMON_PREFIX "/vendor/bin/hw/android.hardware.biometrics.fingerprint"
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware.display.composer-service"
+#define ZYGOTE32_BIN "/system/bin/app_process32"
+#define ZYGOTE64_BIN "/system/bin/app_process64"
+static struct signal_struct *zygote32_sig;
+static struct signal_struct *zygote64_sig;
+
+bool task_is_zygote(struct task_struct *p)
+{
+	return p->signal == zygote32_sig || p->signal == zygote64_sig;
+}
 
 void __register_binfmt(struct linux_binfmt * fmt, int insert)
 {
@@ -1897,6 +1906,10 @@ static int __do_execve_file(int fd, struct filename *filename,
 				unlikely(!strncmp(filename->name, MI_FP_DAEMON_PREFIX,
 					   strlen(MI_FP_DAEMON_PREFIX)))) {
 			fp_daemon = current;
+		} else if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
+			zygote32_sig = current->signal;
+		} else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN))) {
+ 			zygote64_sig = current->signal;
 		}
 	}
 
